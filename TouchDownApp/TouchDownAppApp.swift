4f919369a9222d34fd08b4c154e9f1e0f49d5e7b
Louise Rennick <wddtechnologies@trios.com>
2025-03-03 18:36:19 -0500

    Initial Commit

---
+++ TouchDownApp/TouchDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownAppApp.swift
+//  TouchDownApp
+//
+//  Created by Louise Rennick on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
